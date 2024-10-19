42892a35137042fd812c91214d7ad10b3572227d
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-10-20 00:21:46 +0200

    refresher snippets

---
+++ review/s01_scripts_and_comments.m
@@ -0,0 +1,12 @@
+
+% This is a comment
+
+% s01, in my context, means script 01
+
+clc     % clear command window
+
+% Use semi-colon to suppress output on the command window.
+% Example
+x = 'hello'
+y = 'awesome';
+z = 'world'
