5c15f27feff6201b71ed67ca020eeace71ea7e52
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-10-20 20:32:04 +0200

    How to select matrix elements or generate sequence

---
+++ review/s02_colon_operator.m
@@ -0,0 +1,10 @@
+clc
+
+% Use colon operator to generate a sequence
+% variable = start : optional_stepsize : end
+x = -10 : 10 : 20;
+
+
+% Use colon operator for data selection
+x = rand(9,9)
+y = x(2:3,:)
